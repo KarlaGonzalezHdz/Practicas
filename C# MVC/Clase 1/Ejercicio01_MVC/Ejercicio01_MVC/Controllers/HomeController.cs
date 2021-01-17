c8ddc214eac5c305a0b9dc6997070d6d23d8b2e3
Karla GH <karlahdz.98@outlook.com>
2021-01-17 13:10:49 -0600

    MODELO-VISTA-CONTROLADOR

---
+++ C# MVC/Clase 1/Ejercicio01_MVC/Ejercicio01_MVC/Controllers/HomeController.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Ejercicio01_MVC.Controllers
+{
+    public class Persona
+    {
+        public string Nombre { get; set; }
+        public int Edad { get; set; }
+    }
+    public class HomeController : Controller
+    {
+        public HttpStatusCodeResult Index()
+        {
+            return new HttpStatusCodeResult(301);
+        }
+
+        /*
+        public JsonResult Index()
+        {
+            var persona1 = new Persona() { Nombre="Jose", Edad=10 };
+            var persona2 = new Persona() { Nombre="Juan", Edad=20 };
+
+            List < Persona > Lista = new List<Persona>();
+            Lista.Add(persona1);
+            Lista.Add(persona2);
+            return Json(Lista, JsonRequestBehavior.AllowGet);
+        }
+         
+        */
+
+        /*
+         public ActionResult Index()
+        {
+            //return View();
+
+            return Content("Primera clase de MVC");
+        } 
+
+        */
+
+        /*
+        public RedirectResult Index()
+        {
+            return Redirect("https://www.google.com.mx");
+        }
+
+        */
+
+        /*
+        public RedirectToRouteResult Index()
+        {
+            return RedirectToAction("About", "Home");
+        }
+
+        */
+
+        /*
+        public HttpStatusCodeResult Index()
+        {
+            return new HttpStatusCodeResult(301);
+        }
+        */
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Página de descripción";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        /*
+        public HttpStatusCodeResult Index()
+        {
+            return new HttpStatusCodeResult(404);
+        }
+
+        */
+
+
+        /*
+         * 
+        public ContentResult Index()
+        {
+            return Content("Jose Muñoz");
+        }
+
+        */
+
+
+
+
+
+    }
+}
